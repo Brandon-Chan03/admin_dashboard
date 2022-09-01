414cd95cd31befd0b5f83d336cf4df5799f7b236
Brandon-Chan03 <Brandon.chan0216@gmail.com>
2022-09-01 18:43:27 -0500

    removed unsued components

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-import { BsCurrencyDollar } from 'react-icons/bs';
 import { GoPrimitiveDot } from 'react-icons/go';
 
-import { Stacked, Pie, Button, SparkLine } from "../components";
+import { Stacked, Button, SparkLine } from "../components";
 
-import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { earningData, SparklineAreaData} from '../data/dummy';
 
 import { useStateContext } from '../contexts/ContextProvider';
 
